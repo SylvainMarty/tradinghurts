f45f9cc5b2714b40fb05c70cc2ec7678b0c0c744
Sylvain Marty <10723351+SylvainMarty@users.noreply.github.com>
2018-05-26 10:07:40 +0200

    Initial commit

---
+++ app/src/main/java/co/guidap/tradinghurts/PhoneStateReceiver.java
@@ -0,0 +1,66 @@
+package co.guidap.tradinghurts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import java.util.Objects;
+
+import co.guidap.tradinghurts.callrecorder.CallRecordService;
+
+public class PhoneStateReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "PhoneStateReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (!Objects.equals(intent.getAction(), "android.intent.action.PHONE_STATE")) {
+            return;
+        }
+
+        TelephonyManager mTelephonyMgr = context.getSystemService(TelephonyManager.class);
+        if (mTelephonyMgr != null) {
+            mTelephonyMgr.listen(new CallListener(context), PhoneStateListener.LISTEN_CALL_STATE);
+        }
+    }
+
+    class CallListener extends PhoneStateListener
+    {
+        private Context context;
+
+        public CallListener(Context context) {
+            this.context = context;
+        }
+
+        public void onCallStateChanged(int state, String incomingNumber)
+        {
+            super.onCallStateChanged(state, incomingNumber);
+            switch (state)
+            {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    // Call incoming
+                    Toast.makeText(context, "CALL_STATE_RINGING", Toast.LENGTH_SHORT).show();
+                    break;
+                case TelephonyManager.CALL_STATE_IDLE:
+                    // Call done
+                    Toast.makeText(context, "CALL_STATE_IDLE", Toast.LENGTH_SHORT).show();
+                    Intent stopIntent = new Intent(context, CallRecordService.class);
+                    context.stopService(stopIntent);
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    // Call responded
+                    Intent startIntent = new Intent(context, CallRecordService.class);
+                    startIntent.setAction(CallRecordService.ACTION_START_RECORDING);
+                    startIntent.putExtra(CallRecordService.EXTRA_INCOMING_NUMBER, incomingNumber);
+                    context.startService(startIntent);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
