8a4cf5c9757ac40d237a4b5c2a443fed4eda075e
Sylvain Marty <sylvain.marty@aol.fr>
2018-05-30 01:18:14 +0200

    Readding permission request support on main activity

--- app/src/main/java/co/guidap/tradinghurts/RecordListActivity.java
+++ app/src/main/java/co/guidap/tradinghurts/RecordListActivity.java
@@ -1,9 +1,11 @@
 package co.guidap.tradinghurts;
 
+import android.Manifest;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,11 +20,20 @@ import co.guidap.tradinghurts.ui.RecordListAdapter;
 
 public class RecordListActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSION_GRANTED = 200;
+
     private RecordViewModel mRecordViewModel;
+    private String [] permissions = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CAPTURE_AUDIO_OUTPUT,
+            //Manifest.permission.READ_PRIVILEGED_PHONE_STATE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSION_GRANTED);
         setContentView(R.layout.activity_record_list);
         setTitle(R.string.record_list_title);
 
